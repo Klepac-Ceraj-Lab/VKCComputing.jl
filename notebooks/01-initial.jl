3b8d52cf0b929d4210d328e51b6a624f2ebd167b
Kevin Bonham <kbonham@wellesley.edu>
2022-05-23 11:56:02 -0400

    initial audit

--- notebooks/01-initial.jl
+++ notebooks/01-initial.jl
@@ -1,5 +1,24 @@
-using Conda
-using BiobakeryUtils
+using SeqAudit
+using Airtable
+using DataFrames
 
-BiobakeryUtils.install_deps()
+base = AirBase("appSWOVVdqAi5aT5u")
+stab = AirTable("Samples", base)
+ptab = AirTable("Project", base)
+mgxtab = AirTable("MGX Batches", base)
+metabtab = AirTable("Metabolomics Batches", base)
+ftab = AirTable("Files", base)
 
+recs = Airtable.query(stab; filterByFormula="{Project} = 'ECHO'")
+
+sids = [rec[:sample] for rec in recs if haskey(rec, Symbol("MGX Batches")) && first(rec[Symbol("MGX Batches")]) != "reco4VTTv6UsCRida"]
+#-
+
+filedf = DataFrame(find_analysis_files("/lovelace/sequncing/processed/mgx", sids))
+
+inc = subset(filedf, :status=> ByRow(!=(:complete)))
+sort!(inc, :sample)
+
+transform!(inc, :metaphlan=>ByRow(length)=>:metaphlan_count, :humann=> ByRow(length)=>:humann_count, :kneaddata=>ByRow(length)=> :kneaddata_count)
+
+mis = subset(inc, :status=> ByRow(==(:notfound)))
